c493d3f9707c2a19eb3dcc0f028ccb4f6f17381b
Kozy media <info@kozy.nl>
2018-06-28 20:06:14 +0200

    slightly tidy up search.js

--- src/js/commerce/search.js
+++ src/js/commerce/search.js
@@ -1,37 +1,6 @@
 
 
 const debounceInterval = 600;
-
-const debounce = (fn, time) => {
-  let timeout;
-
-  return function() {
-    const functionCall = () => fn.apply(this, arguments);
-
-    clearTimeout(timeout);
-    timeout = setTimeout(functionCall, time);
-  }
-}
-
-var search = document.getElementById("search");
-var selected_category = document.getElementById("select_industries");
-
-
-const setIndustryAndSearch = (categoryIndex) => {
-  document.getElementById('select_industries').selectedIndex = categoryIndex;
-  doSearch();
-}
-
-const links = document.querySelectorAll('.category_select');
-
-Array.prototype.forEach.call(links, function(el){
-  let industryIndex = el.getAttribute('data-value');
-
-  el.addEventListener("click", (() => {
-    setIndustryAndSearch(industryIndex);
-  }).bind(this));
-});
-
 const summaryInclude=60;
 
 var fuseOptions = {
@@ -47,131 +16,159 @@ var fuseOptions = {
     { name:"name",weight:0.8 },
     { name:"short_description",weight:0.5 },
     { name:"industry", weight:0.3 },
-  ]};
+]};
 
-  function render(templateString, data) {
-    var conditionalMatches,conditionalPattern,copy;
-    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
-    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
-    copy = templateString;
-    while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-      if(data[conditionalMatches[1]]){
-        //valid key, remove conditionals, leave contents.
-        copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
-      } else {
-        //not valid, remove entire section
-        copy = copy.replace(conditionalMatches[0],'');
-      }
-    }
-    templateString = copy;
-    //now any conditionals removed we can do simple substitution
-    var key, find, re;
-    for (key in data) {
-      find = '\\$\\{\\s*' + key + '\\s*\\}';
-      re = new RegExp(find, 'g');
-      templateString = templateString.replace(re, data[key]);
-    }
-    return templateString;
+const search = document.getElementById("search");
+const selected_category = document.getElementById("select_industries");
+const links = document.querySelectorAll('.category_select');
+
+let searchQuery ='';
+let pages;
+let fuse;
+
+const debounce = (fn, time) => {
+  let timeout;
+
+  return function() {
+    const functionCall = () => fn.apply(this, arguments);
+
+    clearTimeout(timeout);
+    timeout = setTimeout(functionCall, time);
   }
+}
 
-  function populateResults(result){
-    $('#search-results').empty();
+const setIndustryAndSearch = (categoryIndex) => {
+  document.getElementById('select_industries').selectedIndex = categoryIndex;
+  doSearch();
+}
 
-    $.each(result,function(key,value){
-      var contents = value.item.short_description;
-      var snippet = "";
-      var snippetHighlights=[];
-      var tags =[];
-      if( fuseOptions.tokenize ){
-        snippetHighlights.push(searchQuery);
-      }else{
-        $.each(value.matches,function(matchKey,mvalue){
-          if(mvalue.key == "tags" || mvalue.key == "industry" ){
-            snippetHighlights.push(mvalue.value);
-          }else if(mvalue.key == "contents"){
-            start = mvalue.indices[0][0]-summaryInclude>0?mvalue.indices[0][0]-summaryInclude:0;
-            end = mvalue.indices[0][1]+summaryInclude<contents.length?mvalue.indices[0][1]+summaryInclude:contents.length;
-            snippet += contents.substring(start,end);
-            snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1]-mvalue.indices[0][0]+1));
-          }
-        });
-      }
+Array.prototype.forEach.call(links, function(el){
+  let industryIndex = el.getAttribute('data-value');
 
-      if(snippet.length<1){
-        snippet += contents.substring(0,summaryInclude*2);
-      }
-      //pull template from hugo templarte definition
-      var templateDefinition = $('#search-result-template').html();
-      //replace values
-      var output = render(templateDefinition,{key:key,name:value.item.name,link:value.item.permalink,tags:value.item.tags,industry:value.item.industry,snippet:snippet,logo:value.item.logo});
-      $('#search-results').append(output);
+  el.addEventListener("click", (() => {
+    setIndustryAndSearch(industryIndex);
+  }).bind(this));
+});
 
-      $.each(snippetHighlights,function(snipkey,snipvalue){
-        $("#summary-"+key).mark(snipvalue);
+function render(templateString, data) {
+  var conditionalMatches,conditionalPattern,copy;
+  conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
+  //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
+  copy = templateString;
+  while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
+    if(data[conditionalMatches[1]]){
+      //valid key, remove conditionals, leave contents.
+      copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
+    } else {
+      //not valid, remove entire section
+      copy = copy.replace(conditionalMatches[0],'');
+    }
+  }
+  templateString = copy;
+  //now any conditionals removed we can do simple substitution
+  var key, find, re;
+  for (key in data) {
+    find = '\\$\\{\\s*' + key + '\\s*\\}';
+    re = new RegExp(find, 'g');
+    templateString = templateString.replace(re, data[key]);
+  }
+  return templateString;
+}
+
+function populateResults(result){
+  $('#search-results').empty();
+
+  $.each(result,function(key,value){
+    var contents = value.item.short_description;
+    var snippet = "";
+    var snippetHighlights=[];
+    var tags =[];
+    if( fuseOptions.tokenize ){
+      snippetHighlights.push(searchQuery);
+    }else{
+      $.each(value.matches,function(matchKey,mvalue){
+        if(mvalue.key == "tags" || mvalue.key == "industry" ){
+          snippetHighlights.push(mvalue.value);
+        }else if(mvalue.key == "contents"){
+          start = mvalue.indices[0][0]-summaryInclude>0?mvalue.indices[0][0]-summaryInclude:0;
+          end = mvalue.indices[0][1]+summaryInclude<contents.length?mvalue.indices[0][1]+summaryInclude:contents.length;
+          snippet += contents.substring(start,end);
+          snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1]-mvalue.indices[0][0]+1));
+        }
       });
-
-    });
-  }
-
-  let searchQuery ='';
-  let pages;
-  let fuse;
-
-  let doSearch = () => {
-
-    if(selected_category.selectedIndex !== 0) {
-      searchQuery = `${selected_category.value} `;
     }
 
-    if(+search.value !== 0) {
-      searchQuery += search.value;
+    if(snippet.length<1){
+      snippet += contents.substring(0,summaryInclude*2);
     }
+    //pull template from hugo templarte definition
+    var templateDefinition = $('#search-result-template').html();
+    //replace values
+    var output = render(templateDefinition,{key:key,name:value.item.name,link:value.item.permalink,tags:value.item.tags,industry:value.item.industry,snippet:snippet,logo:value.item.logo});
+    $('#search-results').append(output);
 
-    if(searchQuery.length > 0) {
-
-      var result = fuse.search(searchQuery);
-
-      if(result.length > 0){
-        populateResults(result);
-      } else {
-        $('#search-results').append("<p>No matches found</p>");
-      }
-    }
-    searchQuery = '';
-  }
-
-  const showAll = (data) => {
-    const everything = data.map(function(value, index){
-      return { item: value, matches: [] };
+    $.each(snippetHighlights,function(snipkey,snipvalue){
+      $("#summary-"+key).mark(snipvalue);
     });
 
-    populateResults(everything);
-  };
+  });
+}
 
-  const initialiseSearch = () => {
-    $.getJSON( "index.json", (data) => {
-      pages = data.data;
+let doSearch = () => {
 
-      showAll(pages);
-
-      fuse = new Fuse(pages, fuseOptions);
-
-      search.setAttribute('placeholder', 'search terms');
-
-      search.removeAttribute('disabled');
-      selected_category.removeAttribute('disabled');
-
-      search.focus();
-
-      selected_category.onchange = doSearch;
-      search.oninput = debounce(doSearch, debounceInterval);
-
-    });
+  if(selected_category.selectedIndex !== 0) {
+    searchQuery = `${selected_category.value} `;
   }
 
-  if (document.readyState !== 'loading') {
-    const search_form = document.getElementById("member-search");
-    if(search_form) initialiseSearch();
-  } else {
-    document.addEventListener('DOMContentLoaded', eventHandler);
+  if(+search.value !== 0) {
+    searchQuery += search.value;
   }
+
+  if(searchQuery.length > 0) {
+
+    var result = fuse.search(searchQuery);
+
+    if(result.length > 0){
+      populateResults(result);
+    } else {
+      $('#search-results').append("<p>No matches found</p>");
+    }
+  }
+  searchQuery = '';
+}
+
+const showAll = (data) => {
+  const everything = data.map(function(value, index){
+    return { item: value, matches: [] };
+  });
+
+  populateResults(everything);
+};
+
+const initialiseSearch = () => {
+  $.getJSON( "index.json", (data) => {
+    pages = data.data;
+
+    showAll(pages);
+
+    fuse = new Fuse(pages, fuseOptions);
+
+    search.setAttribute('placeholder', 'search terms');
+
+    search.removeAttribute('disabled');
+    selected_category.removeAttribute('disabled');
+
+    search.focus();
+
+    selected_category.onchange = doSearch;
+    search.oninput = debounce(doSearch, debounceInterval);
+
+  });
+}
+
+if (document.readyState !== 'loading') {
+  const search_form = document.getElementById("member-search");
+  if(search_form) initialiseSearch();
+} else {
+  document.addEventListener('DOMContentLoaded', eventHandler);
+}
