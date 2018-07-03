fab89d8f58abfed30c593f4c39d81557d21b51b2
Kozy media <info@kozy.nl>
2018-07-03 17:41:07 +0200

    replace div contents on nothing found

--- src/js/commerce/search.js
+++ src/js/commerce/search.js
@@ -136,7 +136,7 @@ let doSearch = () => {
     if(result.length > 0){
       populateResults(result);
     } else {
-      $('#search-results').append("<p>No matches found</p>");
+      $('#search-results').html("<p>No matches found</p>");
     }
   } else {
     showAll(data);
